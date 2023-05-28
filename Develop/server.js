91adbf170a2597d05edaf1d056360b0a4eb0f19c
SaintOfMirren <drew.miller2015@gmail.com>
2023-05-28 13:21:19 -0700

    tried to fix route issue where server is not responding with 404 error

--- Develop/server.js
+++ Develop/server.js
@@ -6,10 +6,11 @@ const clog = require('./middleware/clog');
 const app = express();
 const port = 3000;
 
+app.use(clog);
+
 const routes = require('./routes/index');
 
 app.use(bodyParser.json());
-app.use(clog);
 app.use('/api', routes);
 
 app.listen(port, () => {
